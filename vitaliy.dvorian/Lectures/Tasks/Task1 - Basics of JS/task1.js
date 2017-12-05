991fdf42b8c689d09934a5209b5fbd61a709d781
Vitaliy Dvoryan <v.dvoryan@gmail.com>
2017-12-05 15:11:10 +0200

    Update task1.js

--- vitaliy.dvorian/Lectures/Tasks/Task1 - Basics of JS/task1.js
+++ vitaliy.dvorian/Lectures/Tasks/Task1 - Basics of JS/task1.js
@@ -1,19 +1,25 @@
 //initialize variables
-let textArea = document.querySelector('textarea');
-let calc = document.querySelector('button');
-let text; //an array of words
-let reg = /\s+/g;
-let i;
-let paragraph = document.querySelector('p'); 
-let info; //result
-let numOfWords, maxWord, minWord, averageWord // show
+let textArea = document.getElementById('textBox'),
+ calcBtn = document.getElementById('calc'),
+ showNumberWords = document.getElementById('numOfWords'),
+ showlengthMax = document.getElementById('lengthMax'),
+ showlengthMin = document.getElementById('lengthMin'),
+ showAverage = document.getElementById('average'),
+ text, //an array of words
+ reg = /\s+/g,
+ i,
+ info, //result
+ numOfWords, maxWord, minWord, averageWord; // show
+
+
+calcBtn.addEventListener("click", calculate);
 
 /*  
     the function calculates the number of letters, 
     the length of the largest and the smallest words, 
     and the average length of the word
 */
-calc.onclick = function(){
+function calculate(){
     //parsing string
     text = textArea.value.replace(/,|\.(?=\s+)/g,'');  //convert "123, 456. 1....2" => "123 456 1....2"
     text = text.split(reg);   //convert a string to an array
@@ -23,17 +29,16 @@ calc.onclick = function(){
         }
     }
     // program logic
-    numOfWords = text.join('').length; 
+    numOfWords = text.length; 
     maxWord = Math.max(...(text.map(el => el.length))); // search for the longest word
     minWord = Math.min(...(text.map(el => el.length))); // search for the shortest word
-    averageWord = Math.round( numOfWords / text.length ); //average value
+    averageWord = Math.round( text.join('').length / numOfWords ); //average value
     
     // show
-    str = `Number of words: <${numOfWords}> <br>
-           Length of the maximal word: <${maxWord}> <br>
-           Length of the minimum word: <${minWord}> <br>
-           Average length of words: <${averageWord}>`;
-    paragraph.innerHTML = str;
+    showNumberWords.innerHTML = numOfWords;
+    showlengthMax.innerHTML = maxWord;
+    showlengthMin.innerHTML = minWord;
+    showAverage.innerHTML = averageWord;
 }
 
 
